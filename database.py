453b646200d2285be5720f751c5541dfeb4603c0
phillem <philippe.lh12@gmail.com>
2019-03-22 11:23:30 -0400

    Code reformaté

--- database.py
+++ database.py
@@ -1,13 +1,11 @@
-
 from flask_sqlalchemy import SQLAlchemy
 from app import app
 from sqlalchemy import Table, Column, Integer, ForeignKey
 
-
-
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -16,7 +14,3 @@ class User(db.Model):
     password = db.Column(db.String(80))
     nombre_aleatoire = db.Column(db.Integer)
     role = db.Column(db.String(50))
-
-
-
-
